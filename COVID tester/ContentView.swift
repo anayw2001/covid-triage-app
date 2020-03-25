8d771a5cd60d687bf1eeb0179ed0d99c10e1c581
Anay Wadhera <anay1018@gmail.com>
2020-03-24 19:34:07 -0700

    Initial Commit

---
+++ COVID tester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID tester
+//
+//  Created by Anay Wadhera on 3/24/20.
+//  Copyright © 2020 Anay Wadhera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
