c1848aa674016b68f0479f1ef0d0bd9fc3c58948
Anay Wadhera <anay1018@gmail.com>
2020-03-25 20:32:43 -0700

    Add our finishing views.

---
+++ COVID tester/DoNotTestView.swift
@@ -0,0 +1,28 @@
+//
+//  DoNotTestView.swift
+//  COVID tester
+//
+//  Created by Anay Wadhera on 3/25/20.
+//  Copyright © 2020 Anay Wadhera. All rights reserved.
+//
+
+import SwiftUI
+
+struct DoNotTestView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle().fill(Color.red).frame(width: 150, height: 150)
+                Rectangle().fill(Color.white).frame(width:25, height: 110).rotationEffect(.degrees(-45))
+                Rectangle().fill(Color.white).frame(width:25, height: 110).rotationEffect(.degrees(45))
+            }
+            Text("Do not test this patient.").bold()
+        }
+    }
+}
+
+struct DoNotTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoNotTestView()
+    }
+}
---
+++ COVID tester/TestView.swift
@@ -0,0 +1,28 @@
+//
+//  TestView.swift
+//  COVID tester
+//
+//  Created by Anay Wadhera on 3/25/20.
+//  Copyright © 2020 Anay Wadhera. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle().fill(Color.green).frame(width: 150, height: 150)
+                Rectangle().fill(Color.white).frame(width:25, height: 40).rotationEffect(.degrees(-45)).offset(x: -40, y: 10)
+                Rectangle().fill(Color.white).frame(width:25, height: 110).rotationEffect(.degrees(45)).offset(x: 10)
+            }
+            Text("Test this patient ASAP.").bold()
+        }
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
