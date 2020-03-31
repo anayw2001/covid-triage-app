a07fa8e628b8e127af7b388cf949c3b066c065bc
Anay Wadhera <awadhera@berkeley.edu>
2020-03-31 14:32:04 -0700

    tester: update package name and assets

---
+++ COVID tester/HealthCareInterventionGrid.swift
@@ -0,0 +1,9 @@
+//
+//  HealthCareInterventionGrid.swift
+//  COVID tester
+//
+//  Created by Akhil Wadhera Old Mac on 3/31/20.
+//  Copyright © 2020 Anay Wadhera. All rights reserved.
+//
+
+import Foundation
